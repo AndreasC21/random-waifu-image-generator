ad45156b0155eefc9c0cb6751cb286b2ccb33ffd
unknown <andreascalvin21@gmail.com>
2024-10-31 00:18:10 +0700

    initial release

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiUrl = "https://api.waifu.im/search";
+const params = {
+  included_tags: ["waifu"],
+};
+
+function addWaifu() {
+  fetch(buildRequestUrl())
+    .then((response) => response.json())
+    .then((data) => {
+      const imageContainer = document.createElement("div");
+      imageContainer.className = "image";
+
+      const image = document.createElement("img");
+      image.src = data.images[0].url;
+
+      imageContainer.appendChild(image);
+
+      document.getElementById("gallery").appendChild(imageContainer);
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error.message);
+    });
+}
+
+function buildRequestUrl() {
+  const queryParams = new URLSearchParams();
+  for (const key in params) {
+    if (Array.isArray(params[key])) {
+      params[key].forEach((value) => {
+        queryParams.append(key, value);
+      });
+    } else {
+      queryParams.set(key, params[key]);
+    }
+  }
+  return `${apiUrl}?${queryParams.toString()}`;
+}
